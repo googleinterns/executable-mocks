77304c091d8091032630649b870913057f6ef33b
Maria Prat Colomer <44071714+mariaprat@users.noreply.github.com>
2020-08-07 15:13:47 +0200

    Address comments on style (#6)
    We address some style issues in the mock utilities. In particular, we merge some lines, close files and move a constant outside of a function.
    
    By @sabinadayanova and @mariaprat.

--- example-codes/fakeflights.go
+++ example-codes/fakeflights.go
@@ -5,13 +5,14 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"os"
-	"io/ioutil"
-	"crypto/sha256"
+    "fmt"
+    "io"
+    "os"
+    "io/ioutil"
+    "crypto/sha256"
 )
 
+const bufferSize = 4096
 // The number and order of the input files has to match the content of the inputHashes slice.
 var inputHashes = []string {
 "62938dad5d3e29ec0ae836ed76cc290da742b4addbf06e3b6ec961cc720552f0",
@@ -19,35 +20,35 @@ var inputHashes = []string {
 "9a8fa186921bd5b92822f8afc868674c88be04f4e720925fc68e5784c3faafed"}
 
 func verifyInputFiles() {
-	for i := 0; i < len(inputHashes); i++ {
-		fIn, err := os.Open(os.Args[5 + i])
-		if err != nil {
-			os.Exit(1)
-		}
-		hash := sha256.New();
-       		buf := make([]byte, 4096)
-       		_, err = io.CopyBuffer(hash, fIn, buf)
-      		if err != nil {
-      			os.Exit(1)
-      		}
-       		if fmt.Sprintf("%x", hash.Sum(nil)) != inputHashes[i] { 
-                	os.Exit(1)
-      		}
-	}
+    for i := 0; i < len(inputHashes); i++ {
+        fIn, err := os.Open(os.Args[5 + i])
+        if err != nil {
+            os.Exit(1)
+        }
+        defer fIn.Close()
+
+        hash := sha256.New()
+        if _, err = io.CopyBuffer(hash, fIn,  make([]byte, bufferSize)); err != nil {
+            os.Exit(1)
+        }
+        if fmt.Sprintf("%x", hash.Sum(nil)) != inputHashes[i] { 
+            os.Exit(1)
+        }
+    }
 }
 
 func generateOutputFile() {
-	err := ioutil.WriteFile(os.Args[4], []byte("I'm an output file.\n"), 0666)
-	if err != nil {
-		os.Exit(1)
-	}
+    err := ioutil.WriteFile(os.Args[4], []byte("I'm an output file.\n"), 0666)
+    if err != nil {
+        os.Exit(1)
+    }
 }
 
 func main() {
-	if len(os.Args) != (5 + len(inputHashes)) || os.Args[1] != "--cull-time" || os.Args[2] != "202007210000" || os.Args[3] != "-o" {
-		os.Exit(1)
-	}
-	verifyInputFiles()
-	generateOutputFile()
-	os.Exit(0)
+    if len(os.Args) != (5 + len(inputHashes)) || os.Args[1] != "--cull-time" || os.Args[2] != "202007210000" || os.Args[3] != "-o" {
+        os.Exit(1)
+    }
+    verifyInputFiles()
+    generateOutputFile()
+    os.Exit(0)
 }
--- example-codes/fakessim2qff.go
+++ example-codes/fakessim2qff.go
@@ -4,43 +4,44 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"os"
-	"io/ioutil"
-	"crypto/sha256"
+    "fmt"
+    "io"
+    "os"
+    "io/ioutil"
+    "crypto/sha256"
 )
 
+const bufferSize = 4096
 var inputHash string = "273fec409f32bd0ecbd50d3ab9acf1406efb85f24c89a257e8a3ad3eab197046"
 
 func verifyInput() {
-	fIn, err := os.Open(os.Args[6])
-	if err != nil  {
-		os.Exit(1) 
-	}
-	hash := sha256.New();
-	buf := make([]byte, 4096)
-	_, err = io.CopyBuffer(hash, fIn, buf)
-	if err != nil {
-		os.Exit(1)
-	}
-	if fmt.Sprintf("%x", hash.Sum(nil)) != inputHash { 
-		os.Exit(1)
-	}
+    fIn, err := os.Open(os.Args[6])
+    if err != nil  {
+        os.Exit(1) 
+    }
+    defer fIn.Close()
+
+    hash := sha256.New()
+    if _, err = io.CopyBuffer(hash, fIn, make([]byte, bufferSize)); err != nil {
+        os.Exit(1)
+    }
+    if fmt.Sprintf("%x", hash.Sum(nil)) != inputHash { 
+        os.Exit(1)
+    }
 }
 
 func generateOutput() {
-	err := ioutil.WriteFile(os.Args[4], []byte("I'm an output file.\n"), 0666) // TODO: Write right content
+    err := ioutil.WriteFile(os.Args[4], []byte("I'm an output file.\n"), 0666) // TODO: Write right content
         if err != nil {
-		os.Exit(1)
-	}
+        os.Exit(1)
+    }
 }
 
 func main() {
-	if len(os.Args) != 7 || os.Args[1] != "-t" || os.Args[2] != "201809010005" || os.Args[3] != "-o" || os.Args[5] != "-d"  {
-        	os.Exit(1)
-	}
-	verifyInput()
-	generateOutput()
-	os.Exit(0)
+    if len(os.Args) != 7 || os.Args[1] != "-t" || os.Args[2] != "201809010005" || os.Args[3] != "-o" || os.Args[5] != "-d"  {
+   	 os.Exit(1)
+    }
+    verifyInput()
+    generateOutput()
+    os.Exit(0)
 }
