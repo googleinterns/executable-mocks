4ace7e87f873bca3d7caa9d5f383482d37b83336
Régis Blanc <regb@google.com>
2020-09-17 11:27:09 +0200

    add source headers for cmd/ configs/ and examples/

--- examples/mocks/util1/main.go
+++ examples/mocks/util1/main.go
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // This is a binary that is like util1, but faster.
 //
 // Currently it only supports running >>util1 --cull-time 202007210000 -o output.dat input1.dat input2.dat input3.dat>>
