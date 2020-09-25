7675bfa0ef88eed44a7ee00b01350e5257a3b8cd
Maria Prat Colomer <44071714+mariaprat@users.noreply.github.com>
2020-09-25 12:04:37 +0200

    Update compress-mocks.sh

--- examples/tar/compress-mocks.sh
+++ examples/tar/compress-mocks.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo "Compressing files..."
-cmd/mockexec/mockexec tmp/tar-test1.textproto -cf tmp/output1.tar examples/tar/data/input1 examples/tar/data/input2echo
+cmd/mockexec/mockexec tmp/tar-test1.textproto -czf tmp/output1.tar examples/tar/data/input1 examples/tar/data/input2
 echo "Files compressed."
